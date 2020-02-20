06e749b286ea4d3bb401fb2b23a73ba4e2a3e4e6
Marissa Kivi <Marissa.Kivi@ie.ibm.com>
2020-02-20 08:44:11 -0600

    adding notes for SDA workflow - tree.ring.input

--- VM_scripts/tree.ring.input.R
+++ VM_scripts/tree.ring.input.R
@@ -3,49 +3,86 @@
 # Author: Ann Raiho, Marissa Kivi 
 # Last modified: January 2020
 
-# This script is composed of two different parts. The first part takes data for all species and summarizes the data, so as to determine
-# the top 98% species for the site in question. The second part reduces the results to only include results for the top 98% species and formats
-# results into an SDA-usable format. 
+# This script has two purposes: 
+# (1) Determine the 98% species for the site based on the tree ring data. 
+# (2) Process the estimated biomass estimates into species-level mean and covariance
+# matrices. 
 
 ################################################################################
 ################################################################################
 ################################################################################
 
-## Part I: Determine 98% species for site
+## Part I: Data formatting
 
 # set up working environment
 library(plyr)
 library(dplyr)
-library(PEcAn.settings)
-library(PEcAn.DB)
-library(PEcAn.data.land)
+#library(PEcAn.settings)
+#library(PEcAn.DB)
+#library(PEcAn.data.land)
 library(ggplot2)
 
 rm(list=ls())
-site = 'NORTHROUND'
 
-# load and prepare data for all species
-input <- readRDS(paste0('/Users/marissakivi/Desktop/PalEON/SDA/sites/',site,'/NPP_STAT_MODEL_',site,'.RDS'))
+### ADJUST VARIABLES HERE
+site = 'HARVARD'
 
-# keep only last 250 iterations - this step isn't always necessary
-sort(unique(input$iter))
-if (site == 'GOOSE') input <- input %>% filter(iter >= 1250)
+# input RDS data file location and name
+input = readRDS(paste0('/Users/marissakivi/Desktop/PalEON/SDA/sites/',site,'/NPP_STAT_MODEL_',site,'.RDS'))
 
-# adjust "type" variable for consistency across sites
-levels(input$type)
-levels(input$type)[1] <- 'ab'
-levels(input$type)[2] <- 'abi'
+# 98% plot save location and name
+plot.loc = paste0('/Users/marissakivi/Desktop/PalEON/SDA/sites/',site,'/sda_priority_spp_plot12.jpg')
 
-# condense input to only include species we are interested in and simplify dataframe
-# non-NA values, one model, total aboveground biomass as variable
-if (site == 'HARVARD'){
-  input = input %>% filter(type == 'ab', model == 'Model RW + Census', !is.na(value)) %>% select(-model, -site_id, -type)
-}else{
-  input = input %>% filter(type == 'ab', !is.na(value)) %>% select(-model, -type)
+# final data product save location
+output = paste0('/Users/marissakivi/Desktop/PalEON/SDA/sites/',site,'/sda.obs.',site,'.plots34.Rdata')
+###
+
+# Before anything else, let's remove all NA values in order to reduce variable size.
+input <- input %>% filter(!is.na(value))
+
+########### 1. Are there only 250 iterations included? ###########
+# The NPP stat model produces thousands of iterations of AGB estimates for each site. However, for
+# our purposes, there was an agreement that we really only need to keep the last 250 iterations for
+# each site. Here we make sure this stays consistent for all sites.
+max.iter = max(input$iter)
+if (max.iter > 250){
+  first = max.iter - 250
+  input <- input %>% filter(iter > first) %>% mutate(iter = iter - first)
 }
 
-# for North Round Pond site, we need to separate the data for Plots 1 & 2 and Plots 3 & 4 
-if (site == 'NORTHROUND') input <- input %>% filter(plot %in% c(1,2)) %>% dplyr::select(-plot)
+########### 2. Does the file include the right data types? ###########
+# Here we adjust "type" variable for consistency across sites.
+levels(input$type) # Do we see two data types? Some variation of AB (1) and ABI (2) ?
+levels(input$type)[1] <- 'ab'
+levels(input$type)[2] <- 'abi'
+# Then we reduce our data to only include "ab" data.
+input = input %>% filter(type == 'ab') %>% select(-type)
+
+########### 3. Which models are available for the site? ###########
+# First, look at the number of models used. If more than one, which model do you want to use?
+levels(input$model)
+model.pick = 'Model RW + Census'
+# Then, reduce the input data to be just that model.
+input = input %>% filter(model == model.pick) %>% select(-model)
+
+########### 4. Which plots do we want to include? ###########
+# On rare occasions, we want to only include data for some of the plots
+# with available data at a site. For example, North Round Pond has plots 
+# with two distinct stand structures and disturbance histories, so we split 
+# them up. If you want to do all the plots, you can skip this step.
+# First, we need to consider the name of the column that gives the plot number for the estimate.
+names(input)
+names(input)[3] = 'plot'
+# Choose which plots you want to include. 
+unique(input$plot)
+plts = c(1,2,3,4)
+input <- input %>% filter(plot %in% plts) %>% select(-plot)
+
+################################################################################
+################################################################################
+################################################################################
+
+## Part II: Determining 98% species
 
 # set up variables for aggregation step
 start_date = min(input$year)
@@ -91,53 +128,56 @@ pl = ggplot(prior_mat) +
   geom_point(aes(x=taxon, y=cumsum)) + 
   geom_hline(yintercept = 0.98, col = 'red') + 
   labs(title = 'Overall Cumulative Proportion of Biomass by Species', 
-       ylab = 'Cumulative Proportion of Biomass')
-ggsave(pl, filename = paste0('/Users/marissakivi/Desktop/PalEON/SDA/sites/',site,'/sda_priority_spp_plot12.jpg'))
+       y = 'Cumulative Proportion of Biomass')
 pl
 
-# determine which species to keep for modeling (top 98% species are under red line)
-species = c('TSCA','QURU','PIST','ACRU')
+######################## STOP ########################
+
+# looking at the plot, record in the following vector the codes of the species whose points fall
+# under the red line 
+species = c('QURU','ACRU','FAGR','BEAL','TSCA')
+
+# save plot
+ggsave(pl, filename = plot.loc)
 
 ################################################################################
 ################################################################################
 ################################################################################
 
-## Part II: Reduce and reformat results for running SDA at site 
+## Part III: Reduce and reformat results for running SDA in PEcAn
 
-# first, reduce data frame to only include top 98% taxa 
+# First, reduce data frame to only include top 98% taxa. 
 melt.next = melt.next %>% filter(taxon %in% species)
 unique(melt.next$taxon)
 
-# are all of these species in the BETY database for LINKAGES? 
+# Now, we need to label the data for each species in a way that PEcAn will recognize so we 
+# can successfully match the data to the model results at the species-level.
+# If you're unsure what species the code refers to, you can checkout the ReadMe file for the RDS.
 
-# make sure taxa codes are same as those found in LINKAGES spp_matrix.csv file 
-# 1.) this only needs to be done for the species in top 98% and is necessary to ensure there
-# are matches for the BETY database
-# 2.) check the README file for RDS file for site to double check common species names
-# 3.) THIS SECTION WILL NEED TO BE ADJUSTED FOR ALL SITES 
-levels(melt.next$taxon)
-#levels(melt.next$taxon)[3] = 'BEAL2' # HF, NRP
-#levels(melt.next$taxon)[2] = 'ACSA3' # NRP
-#levels(melt.next$taxon)[7] = 'FRAM2' # NRP
-#levels(melt.next$taxon)[4] = 'PIRU' # RH
-species = unique(melt.next$taxon)
-species
+# We will label each species with a PFT name that will remain consistent for each species
+# throughout your analysis. Traditionally, for LINKAGES, they follow the pattern: 
+# Genus.Species_Common.Name. However, as you will learn later, we need to slightly vary this 
+# pattern so as to keep your PFTs/priors distinct from the work of others. A few possibilities 
+# include Genus.Species_Common.Name.YOURINITIALS or Genus.Species_Common.Name.VERSIONNUMBER. 
+# Please note, I already used Genus.Species_Common.Name.2 for my analysis. You will see this later.
 
-################################################################################
-################################################################################
-################################################################################
+# Once you choose your pattern, you will need to fill in the following vector with the chosen PFT
+# name for each of your species IN THE SAME ORDER AS THE SPECIES VECTOR ABOVE. 
 
-## Part III. Match up species to PFT on BETY database
-
-# You will need to look at the PFT names that you are going to be using in the BETY 
-# database and match them to the species. These names should be in your priors CSV 
-# file! Traditionally, for LINKAGES, they follow the pattern: Genus.Species_Common.Name
-
+species # for order 
 x = c('Quercus.Rubra_Northern.Red.Oak.2',
-      'Pinus.Strobus_White.Pine.2',
       'Acer.Rubrum_Red.Maple.2',
+      'Fagus.Grandifolia_American.Beech.2',
+      'Betula.Alleghaniensis_Yellow.Birch.2',
       'Tsuga.Canadensis_Hemlock.2')
 names(x) = species
+x
+
+################################################################################
+################################################################################
+################################################################################
+
+## Part IV. Finish up processing of species-level biomass mean and covariance matrices
 
 # adjust names of taxa to be with PFT name from BETY 
 melt.next$pft.cat <- x[as.character(melt.next$taxon)]
@@ -150,9 +190,8 @@ cov.test <- apply(iter_mat,3,function(x){cov(x)})
 arguments2 <- list(.(year, pft.cat), .(variable))
 mean_mat <- reshape2::dcast(melt.next, arguments2, mean)
 
-# Step 3: Correct formatting of input 
-
-# organize results into SDA usable format
+# Organize results into SDA usable format. Note: you will get some warnings after running
+# this section. Don't worry that's normal. 
 for(t in seq_along(obs.times)){
   
   # first mean matrices
@@ -178,17 +217,19 @@ for(t in seq_along(obs.times)){
 obs.mean <- obs.mean.tmp
 obs.cov <- obs.cov.tmp
 
+# This data format is necessary for the output to work with the SDA workflow
 names(obs.mean) <- paste0(obs.times,'-12-31')
 names(obs.cov) <- paste0(obs.times,'-12-31')
 
+# Save the final obs.list 
 obs.list <- list(obs.mean = obs.mean, obs.cov = obs.cov)
-save(obs.list,file=paste0('/Users/marissakivi/Desktop/PalEON/SDA/sites/',site,'/sda.obs.',site,'.plots34.Rdata'))
+save(obs.list,file=output)
 
 ################################################################################
 ################################################################################
 ################################################################################
 
-## Visualize results
+## Visualize mean AGB estimates for all species at site over time
 ggplot(mean_mat, aes(x = year, y = value, col = pft.cat)) +
   geom_point() + 
   geom_smooth(aes(group=pft.cat)) + 
